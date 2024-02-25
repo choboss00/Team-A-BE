227cf5ded6146593c261caf695e6ae9482bd4609
Jin_00 <anjdal64@gmail.com>
2024-02-25 11:01:45 +0900

    [feat] #1 - 프로젝트 환경 설정
    프로젝트 기본 세팅을 해주었습니다.
    
    필수라고 생각되는 의존성만 먼저 추가해주었습니다.
    
    - JPA ( jpa 사용 )
    - LOMBOK ( getter, setter 편하게 달기 위함 )
    - DEVTOOLS ( 프로젝트 자동 새로고침 )
    - MYSQL ( DB )
    - WEB ( 서버 실행을 위해 )

---
+++ src/main/java/com/example/shipgofunding/ShipGoFundingApplication.java
@@ -0,0 +1,13 @@
+package com.example.shipgofunding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipGoFundingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShipGoFundingApplication.class, args);
+    }
+
+}
