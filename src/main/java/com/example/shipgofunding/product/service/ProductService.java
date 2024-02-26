b53254a2db5222c31baaa967a0ec9cc7a492d523
Jin_00 <anjdal64@gmail.com>
2024-02-27 00:23:11 +0900

    [feat] #10 - banner 조회 API 작성
    추후에 테스트 코드를 작성하며, 에러가 발생하는 부분을 찾은 뒤 리팩토링 할 예정

--- src/main/java/com/example/shipgofunding/product/service/ProductService.java
+++ src/main/java/com/example/shipgofunding/product/service/ProductService.java
@@ -1,4 +1,32 @@
 package com.example.shipgofunding.product.service;
 
+import com.example.shipgofunding.product.banner.domain.Banner;
+import com.example.shipgofunding.product.banner.repository.BannerJpaRepository;
+import com.example.shipgofunding.product.response.ProductResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Transactional
+@RequiredArgsConstructor
+@Service
 public class ProductService {
+
+    private BannerJpaRepository bannerJpaRepository;
+
+    @Autowired
+    public ProductService(BannerJpaRepository bannerJpaRepository) {
+        this.bannerJpaRepository = bannerJpaRepository;
+    }
+
+    public List<ProductResponse.BannerResponse> getMainBanners() {
+        List<Banner> banners = bannerJpaRepository.findRandomBanners();
+
+        return banners.stream()
+                .map(ProductResponse.BannerResponse::new)
+                .toList();
+    }
 }
