ded9b9fbf90010a31af6b46f1e490e94c8debf54
Jin_00 <anjdal64@gmail.com>
2024-02-28 18:41:07 +0900

    [test] #17 - 로그인을 해야 API 요청이 가는지 확인

--- src/main/java/com/example/shipgofunding/Config/SecurityConfig.java
+++ src/main/java/com/example/shipgofunding/Config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package com.example.shipgofunding.config;
 
+import com.example.shipgofunding.config.jwt.JwtAuthenticationFilter;
 import com.example.shipgofunding.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
@@ -13,16 +16,18 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
 import java.util.Collections;
 
+@EnableMethodSecurity
+@RequiredArgsConstructor
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
-
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;	// JwtAuthenticationFilter 주입
     // 비밀번호 암호화
     @Bean
     public BCryptPasswordEncoder passwordEncoder(){
@@ -46,10 +51,12 @@ public class SecurityConfig {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 사용 X
+                .addFilterBefore(jwtAuthenticationFilter, BasicAuthenticationFilter.class)
                 .cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource())) // CORS 설정
                 .formLogin(AbstractHttpConfigurer::disable) // form login 사용 X
                 .httpBasic(AbstractHttpConfigurer::disable) // http basic 사용 X
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/test/**").authenticated()
                         .anyRequest().permitAll()) // 모든 요청 허용
                 .headers((headers) -> headers
                         .addHeaderWriter(new XFrameOptionsHeaderWriter(
