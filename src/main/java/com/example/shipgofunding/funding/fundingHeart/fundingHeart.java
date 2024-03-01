25232c7d901c9c4b6f54b962ad29bdfa16ef8f48
Jin_00 <anjdal64@gmail.com>
2024-03-01 11:04:07 +0900

    [feat] #10 - 펀딩 좋아요 역할을 추가하기 위한 도메인 추가

---
+++ src/main/java/com/example/shipgofunding/funding/fundingHeart/fundingHeart.java
@@ -0,0 +1,28 @@
+package com.example.shipgofunding.funding.fundingHeart;
+
+import com.example.shipgofunding.config.utils.MetaData;
+import com.example.shipgofunding.funding.domain.Funding;
+import com.example.shipgofunding.user.domain.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+@SQLRestriction("deleted_at IS NULL")
+@SQLDelete(sql = "UPDATE funding_hearts SET deleted_at = CURRENT_TIMESTAMP, is_deleted = TRUE where id = ?")
+@Entity
+@Table(name = "funding_hearts")
+public class fundingHeart extends MetaData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Funding funding;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+}
