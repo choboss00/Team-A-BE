90eb3d97010c1300bd7e012c75416a16258982c5
Jin_00 <anjdal64@gmail.com>
2024-03-01 10:35:20 +0900

    [refactor] #10 - 마감임박상품 쿼리문 수정
    마감이 종료된 상품은 나오지 않도록 수정

--- src/main/java/com/example/shipgofunding/funding/repository/FundingJpaRepository.java
+++ src/main/java/com/example/shipgofunding/funding/repository/FundingJpaRepository.java
@@ -7,7 +7,11 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface FundingJpaRepository extends JpaRepository<Funding, Integer> {
-    @Query(value = "SELECT * FROM fundings f WHERE f.end_date < CURRENT_TIMESTAMP + INTERVAL '72' HOUR ORDER BY RAND() LIMIT 3", nativeQuery = true)
+    @Query(value = "SELECT * FROM fundings f " +
+            "WHERE f.end_date >= CURRENT_TIMESTAMP AND " +
+            "f.end_date < CURRENT_TIMESTAMP + INTERVAL '72' HOUR " +
+            "ORDER BY RAND() " +
+            "LIMIT 3", nativeQuery = true)
     List<Funding> findRandomFundings();
 
 }
