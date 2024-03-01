9ff0a1d62521c3e5fa7206c231963e6d116ec94c
Jin_00 <anjdal64@gmail.com>
2024-03-01 10:36:07 +0900

    [refactor] #10 - 마감임박상품의 DTO 수정
    시작시간, 종료시간, 현재 남은 시간을 추가

--- src/main/java/com/example/shipgofunding/funding/response/FundingResponse.java
+++ src/main/java/com/example/shipgofunding/funding/response/FundingResponse.java
@@ -6,6 +6,10 @@ import com.example.shipgofunding.funding.image.domain.FundingImage;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class FundingResponse {
 
     @Getter
@@ -26,10 +30,34 @@ public class FundingResponse {
     public static class UrgentFundingResponseDTO {
         private int fundingId;
         private String fundingImage;
+        private String startTime;
+        private String endTime;
+        private String timeRemaining;
 
+        // LocalDateTime을 String으로 포맷하기 위한 formatter 정의
+        private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         public UrgentFundingResponseDTO(FundingImage fundingImage) {
             this.fundingId = fundingImage.getFunding().getId();
             this.fundingImage = fundingImage.getFundingImage();
+            this.startTime = fundingImage.getFunding().getStartDate().format(formatter);
+            this.endTime = fundingImage.getFunding().getEndDate().format(formatter);
+            this.timeRemaining = calculateTimeRemaining(fundingImage.getFunding().getEndDate());
+        }
+
+        // 종료 시간까지 남은 시간을 계산하는 메소드
+        private String calculateTimeRemaining(LocalDateTime endDate) {
+            LocalDateTime now = LocalDateTime.now();
+            Duration duration = Duration.between(now, endDate);
+            long hours = duration.toHours();
+            long minutes = duration.toMinutes() % 60;
+            long seconds = duration.getSeconds() % 60;
+
+            // 남은 시간이 없거나 과거인 경우 "00:00:00"을 반환
+            if (hours < 0) {
+                return "00:00:00";
+            } else {
+                return String.format("%02d:%02d:%02d", hours, minutes, seconds);
+            }
         }
     }
 
