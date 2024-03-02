f9d6ebdc5cf150cbf1b5203260015560167af8bf
Jin_00 <anjdal64@gmail.com>
2024-03-03 08:50:25 +0900

    [feat] #30 - 댓글 도메인 구현

---
+++ src/main/java/com/example/shipgofunding/comment/domain/Comment.java
@@ -0,0 +1,34 @@
+package com.example.shipgofunding.comment.domain;
+
+import com.example.shipgofunding.config.utils.MetaData;
+import com.example.shipgofunding.funding.domain.Funding;
+import com.example.shipgofunding.user.domain.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import static jakarta.persistence.GenerationType;
+
+@Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+@SQLRestriction("deleted_at IS NULL")
+@SQLDelete(sql = "UPDATE comments SET deleted_at = CURRENT_TIMESTAMP, is_deleted = TRUE where id = ?")
+@Entity
+@Table(name = "comments")
+public class Comment extends MetaData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Funding funding;
+
+    @Column(nullable = false)
+    private String content;
+}
