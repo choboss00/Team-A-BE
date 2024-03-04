c08a533d9cc76a0f7de6be3192733ee237eb93c8
Jin_00 <anjdal64@gmail.com>
2024-03-04 17:33:39 +0900

    [feat] #44 - 신청 로직 작성
    현재 시간과 startDate 를 비교 후 데이터를 넣음
    
    예시
    현재 시간 : 3월 7일, startDate : 3월 8일
    -> notificationFunding 데이터 추가
    
    예시2
    현재 시간 : 3월 4일 startDate : 3월 3일
    -> participatingFunding 에 데이터 추가

--- src/main/java/com/example/shipgofunding/funding/controller/FundingController.java
+++ src/main/java/com/example/shipgofunding/funding/controller/FundingController.java
@@ -160,4 +160,12 @@ public class FundingController {
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponseBuilder.successWithNoContent());
     }
 
+    @Operation(summary = "펀딩 상품 신청하기", description = "펀딩 상품에 신청합니다.")
+    @ApiResponse(responseCode = "200", description = "성공적으로 펀딩 상품 신청하기")
+    @PostMapping("/fundings/{fundingId}")
+    public ResponseEntity<?> applyFunding(@PathVariable int fundingId, @AuthenticationPrincipal PrincipalUserDetails userDetails) {
+        fundingService.applyFunding(fundingId, userDetails);
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponseBuilder.success(fundingId));
+    }
+
 }
